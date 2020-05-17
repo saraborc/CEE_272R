f23936b42946cbb7c429623977b98b3d7403fd06
Conner Geery <cgeery@stanford.edu>
2020-05-17 15:46:28 -0700

    adding commented steps

--- finalproj.py
+++ finalproj.py
@@ -7,3 +7,20 @@ Created on Fri May  8 16:37:59 2020
 """
 
 print("YAY!!")
+print("Hi Sara!!") # @cmgeery
+
+# CREATE NETWORK: create network, establish ideal voltages by load type
+
+# ADJUST NETWORK LOADS: bump voltages in various areas according to scenarios
+# SCENARIO 1: increased home charging
+
+# SCENARIO 2: increased quick charging stations
+
+# SCENARIO 3: mix of quick charging and home charging
+
+
+
+# CREATE DEMAND PROFILES: time series calculations/load changes with time
+
+# EVALUATE EV USER CHARGES
+
